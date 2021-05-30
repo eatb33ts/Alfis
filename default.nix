72d6477399617fc718c5420d25be575bd5b66e14
cw <beetr00ts@pm.me>
2021-05-30 01:59:35 -0700

    I will never fucking understand git or it's workflow

--- default.nix
+++ default.nix
@@ -1,21 +1,41 @@
-{ pkgs ? import <nixpkgs> {} }:
+#{ pkgs ? import <nixpkgs> {} }: # poly1305 v0.7.0 (cargo) fails to compile this way
+{ ... }:
 let
-  stdenv = pkgs.stdenv;
-  fetchFromGitHub = pkgs.fetchFromGitHub;
-  rustPlatform = pkgs.rustPlatform;
+  pkgs = import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/a58a0b5098f0c2a389ee70eb69422a052982d990.tar.gz")) {};
 in
+  let
+    stdenv = pkgs.stdenv;
+    fetchFromGitHub = pkgs.fetchFromGitHub;
+    rustPlatform = pkgs.rustPlatform;
+  in
 rustPlatform.buildRustPackage rec {
   pname = "Alfis";
-  version = "v0.5.8b";
+  version = "v0.6.0";
+ 
+  buildInputs = [
+    pkgs.glib
+    #pkgs.cairo
+    #pkgs.pango
+    #pkgs.gdk-pixbuf
+    #pkgs.atk
+    pkgs.gtk3
+    pkgs.libsoup
+    pkgs.webkitgtk
+    pkgs.kdialog
+  ];
+  nativeBuildInputs = [ pkgs.pkg-config ]; #pkgs.webkitgtk pkgs.pkg-config pkgs.kdialog pkgs.cairo pkgs.glibc ];
+
+  doCheck = false;
+  #cargoTestFlags = 
 
   src = fetchFromGitHub {
-    owner = "eatb33ts";
+    owner = "Revertron";
     repo = pname;
     rev = version;
-    sha256 = "1rwxj65zvjq2kh1x0g138hqdgc5hxqf5lqb1g7yxz13wgnm7lc2g";
+    sha256 = "1gdjrqsphb36f0s5j2grkhd0rfgmc76wwsbd0flq124c6kkx0cjs";
   };
 
-  cargoSha256 = "17ldqr3asrdcsh4l29m3b5r37r5d0b3npq1lrgjmxb6vlx6a36qh";
+  cargoSha256 = "0xjcfa3nc87csav55bkk4gl40g7c2b6vxf8z3arn6w48lg3v72r6";
 
   meta = with stdenv.lib; {
     description = "Alternative Free Identity System";
